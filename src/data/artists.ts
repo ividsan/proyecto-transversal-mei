1e94bd923431cfa6c6d03d920c1ae11bae415d1e
Berta Beniel <bertabeniel@gmail.com>
2026-05-28 18:08:25 +0200

    pagina artistas especificos

---
+++ src/data/artists.ts
@@ -0,0 +1,156 @@
+export interface Artist {
+  slug: string
+  name: string
+  fullName: string
+  image: string
+  bio: string
+  instagram: string
+  spotify: string
+}
+
+export const artists: Artist[] = [
+  {
+    slug: "amaia",
+    name: "Amaia",
+    fullName: "Amaia Romero Arbizu",
+    image: "/artistesHome/amaiaEditada.png",
+    bio:
+      "Cantant, pianista i compositora navarresa amb un pop delicat, elegant i molt emocional. Va saltar a primera fila després d'Operación Triunfo i ha consolidat una proposta íntima i molt personal.",
+    instagram: "@amaia",
+    spotify: "Amaia",
+  },
+  {
+    slug: "mushkaa",
+    name: "Mushkaa",
+    fullName: "Irma Farelo i Solé",
+    image: "/artistesHome/mushkaEditada.png",
+    bio:
+      "Una de les veus més potents de l'escena urbana catalana. Barreja reggaeton, trap, R&B i pop amb lletres sinceres i una energia molt pròpia sobre l'escenari.",
+    instagram: "@mushkaaa___",
+    spotify: "Mushkaa",
+  },
+  {
+    slug: "judeline",
+    name: "Judeline",
+    fullName: "Lara Fernández Castrelo",
+    image: "/artistesHome/judelineEditada.png",
+    bio:
+      "Cantant i compositora andalusa que combina pop, influències llatines i textures experimentals. La seva proposta destaca per l'atmosfera i una sensibilitat molt contemporània.",
+    instagram: "@judeli.ne",
+    spotify: "Judeline",
+  },
+  {
+    slug: "els-catarres",
+    name: "Els Catarres",
+    fullName: "Èric Vergés, Jan Riera Prats i Roser Cruells",
+    image: "/artistesHome/catarresEditada.png",
+    bio:
+      "Banda catalana de pop-folk amb tornades festives i molta connexió amb el directe. S'han convertit en un dels noms més estimats de l'escena en català.",
+    instagram: "@elscatarres",
+    spotify: "Els Catarres",
+  },
+  {
+    slug: "carolina-durante",
+    name: "Carolina Durante",
+    fullName: "Diego Ibáñez, Martín Vallhonrat, Juan Pedrayes i Mario del Valle",
+    image: "/artistesHome/carolinaduranteEditada.png",
+    bio:
+      "Banda madrilenya d'indie rock amb lletres afilades, humor sarcàstic i concerts molt enèrgics. La seva proposta és directa, guitarrera i generacional.",
+    instagram: "@carolinadurante_",
+    spotify: "Carolina Durante",
+  },
+  {
+    slug: "julieta",
+    name: "Julieta",
+    fullName: "Julieta Gracián Salas",
+    image: "/artistesHome/julietaEditada.png",
+    bio:
+      "Una de les veus més destacades del nou pop català. Barreja pop, electrònica i ritmes urbans amb una estètica molt cuidada i una sonoritat expansiva.",
+    instagram: "@julietagracian",
+    spotify: "Julieta",
+  },
+  {
+    slug: "la-elite",
+    name: "La Élite",
+    fullName: "Nil Roig i David Burgués",
+    image: "/artistesHome/eliteEditada.png",
+    bio:
+      "Duo català de synth-punk amb actitud irreverent, sintetitzadors contundents i molta energia. Les seves cançons respiren ironia, velocitat i esperit de club.",
+    instagram: "@laeliteig",
+    spotify: "La Élite",
+  },
+  {
+    slug: "jimena-amarillo",
+    name: "Jimena Amarillo",
+    fullName: "Jimena Amarillo Peiró",
+    image: "/artistesHome/jimenaamArilloEditada.png",
+    bio:
+      "Artista valenciana que s'ha fet forta amb un bedroom pop tendre, directe i molt personal. Les seves lletres connecten amb una generació que busca emoció i honestedat.",
+    instagram: "@jimenamarillo",
+    spotify: "Jimena Amarillo",
+  },
+  {
+    slug: "zetak",
+    name: "Zetak",
+    fullName: "Pello Reparaz Escala, Leire Colomo, Iban Larreburu i Gorka Pastor",
+    image: "/artistesHome/zetakEditada.png",
+    bio:
+      "Projecte de pop electrònic en euskera liderat per Pello Reparaz. Uneix identitat, producció moderna i un directe pensat gairebé com una experiència escènica.",
+    instagram: "@zetak",
+    spotify: "Zetak",
+  },
+  {
+    slug: "la-gossa-sorda",
+    name: "La Gossa Sorda",
+    fullName: "Josep Nadal, Àlex Seguí, Cate Giorgi, Salvador Bolufer, Jordi Martí, Arnau Giménez, Mark Dasousa i Héctor Peropadre",
+    image: "/artistesHome/gossaSordaEditada.png",
+    bio:
+      "Grup valencià de referència que barreja rock, punk, reggae i ritmes mediterranis. Les seves cançons combinen festa, compromís i una forta identitat pròpia.",
+    instagram: "@lagossasorda_oficial",
+    spotify: "La Gossa Sorda",
+  },
+  {
+    slug: "sandra-monfort",
+    name: "Sandra Monfort",
+    fullName: "Sandra Monfort Oliver",
+    image: "/artistesHome/sandraMonfortEditada.png",
+    bio:
+      "Artista de Pedreguer que treballa entre la cançó d'arrel, el pop i l'experimentació. La seva música uneix tradició, sensibilitat i una mirada molt actual.",
+    instagram: "@sandramonfort",
+    spotify: "Sandra Monfort",
+  },
+  {
+    slug: "fades",
+    name: "Fades",
+    fullName: "Ferran Pi, Vicenç Calafell i Àngel Exojo",
+    image: "/artistesHome/fadesEditada.png",
+    bio:
+      "Trio mallorquí queer que fa pop, house i música urbana amb una identitat molt marcada. El seu univers és festiu, reivindicatiu i molt generacional.",
+    instagram: "@ffades",
+    spotify: "Fades",
+  },
+  {
+    slug: "allerqiques-al-pollen",
+    name: "Al·lèrgiques al pol·len",
+    fullName: "Maria Riba, Ona Salabert, Berta Batiste, Emma Dotres, Marina Rico i Joana Subirats",
+    image: "/artistesHome/alergiquesAlPolenEditada.jpg",
+    bio:
+      "Projecte de pop en català amb un so directe, juganer i molt melòdic. Les seves cançons combinen frescor, ironia i ganes de pista.",
+    instagram: "@alergiquesalpolen",
+    spotify: "Al·lèrgiques al pol·len",
+  },
+  {
+    slug: "mala-gestion",
+    name: "Mala Gestión",
+    fullName: "Elías, Héctor, Lucas, Guille, Joan i Pablo",
+    image: "/artistesHome/malagestionEditada.png",
+    bio:
+      "Banda valenciana amb esperit punk i indie que destaca per la seva actitud crua i les lletres sense filtres. Els seus directes són intensos i molt contagiosos.",
+    instagram: "@malagestion",
+    spotify: "Mala Gestión",
+  },
+]
+
+export function getArtistBySlug(slug: string) {
+  return artists.find((artist) => artist.slug === slug)
+}
